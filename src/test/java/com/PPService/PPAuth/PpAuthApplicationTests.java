5d74a739d538e70ad303c2fdc8b322949d5c20a7
hh <hhh@gmail.com>
2025-12-11 07:16:40 +1100

    处理工程

---
+++ src/test/java/com/PPService/PPAuth/PpAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PPService.PPAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
