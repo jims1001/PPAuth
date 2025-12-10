0ee93da19e33df743980108eed5f3501b74dfeec
hh <hhh@gmail.com>
2025-12-11 07:12:44 +1100

    创建工程

---
+++ src/main/java/com/pgservice/pjauth/PjauthApplication.java
@@ -0,0 +1,13 @@
+package com.pgservice.pjauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PjauthApplication.class, args);
+	}
+
+}
